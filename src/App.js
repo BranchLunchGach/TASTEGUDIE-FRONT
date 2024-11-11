0b616fa7b77bc55b8b0875cf32bc4df45a6d59af
passing7by <jinam0717@gmail.com>
2024-11-11 19:50:39 +0900

    add : FindIDPWDForm, FindIDPWDPage 추가

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import "./App.css";
 import Navirouter from "./Navirouter";
 import MenuResultPage from "./pages/MenuResultPage";
 import LoginPage from "./pages/user/LoginPage";
+import FindIDPWDPage from "./pages/user/FindIDPWDPage";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <LoginPage />
+        <FindIDPWDPage />
       </BrowserRouter>
     </div>
   );
---
+++ src/pages/user/FindIDPWDPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header2 from "../../components/layouts/Header2";
+import FindIDPWDForm from "../../components/user/FindIDPWDForm";
+
+const LoginPage = () => {
+  return (
+    <>
+      <Header2 />
+      <FindIDPWDForm />
+    </>
+  );
+};
+
+export default LoginPage;
