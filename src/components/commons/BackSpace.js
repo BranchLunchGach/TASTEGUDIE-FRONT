00ca47117989bc57aa00e1a95b560a1c32f0995b
passing7by <jinam0717@gmail.com>
2024-11-13 16:04:13 +0900

    add : 식당 추천 결과 페이지, 샘플 이미지, reactor : BackSpace.js, Retry.js 크기 변경

--- src/components/commons/BackSpace.js
+++ src/components/commons/BackSpace.js
@@ -3,9 +3,9 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 const StyledIcon = styled.img`
-  width: 67px;
-  height: 68px;
-  margin: 70px;
+  width: 60px;
+  height: 60px;
+  margin-right: 30px;
 `;
 
 const BackSpace = () => {
